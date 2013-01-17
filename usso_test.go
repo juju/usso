8788485446f83a36f709811a4a3d0cff7ef583a5
vincenzo di somma <vincenzo.di.somma@canonical.com>
2013-01-17 17:19:27 +0100

    Tests now pass.

--- usso_test.go
+++ usso_test.go
@@ -4,9 +4,95 @@
 package usso
 
 import (
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
 	"testing"
 )
 
+func return_tokens(w http.ResponseWriter, request *http.Request) {
+	// Utility function to test GetToken
+	body, _ := ioutil.ReadAll(request.Body)
+	if string(body) != `{"email":"foo@bar.com","password":"foobarpwd",`+
+		`"token_name":"token_name"}` {
+		log.Fatalf("Wrong Credential sent to server.\n")
+	}
+	io.WriteString(
+		w,
+		`{
+		 "token_name": "foo", 
+		 "date_updated": "2013-01-16 14:03:36", 
+		 "token_key": "abcs", 
+		 "consumer_secret": "rwDkQkkdfdfdeAslkmmxAOjOAT", 
+		 "href": "/api/v2/tokens/abcd", 
+		 "date_created": "2013-01-16 14:03:36", 
+		 "consumer_key": "rfyzhdQ", 
+		 "token_secret": "mTBgLxtTRUdfqewqgrqsvxlijbMWkPBajgKcoZCrDwv"
+		 }`)
+}
+
 func TestGetToken(test *testing.T) {
-	// Test GetToken
+	// Test that GetToken connects to the right
+	http.HandleFunc("/", return_tokens)
+	go http.ListenAndServe(":8123", nil)
+	creds := Credentials{
+		"foo@bar.com",
+		"foobarpwd",
+		"token_name",
+		"http://localhost:8123"}
+	ssodata, _ := GetToken(&creds)
+	if ssodata.ConsumerKey != "rfyzhdQ" {
+		test.Fatalf("Wrong consumer key: %s", ssodata.ConsumerKey)
+	}
+	if ssodata.ConsumerSecret != "rwDkQkkdfdfdeAslkmmxAOjOAT" {
+		test.Fatalf("Wrong consumer secret: %s", ssodata.ConsumerSecret)
+	}
+	if ssodata.TokenKey != "abcs" {
+		test.Fatalf("Wrong token key: %s", ssodata.TokenKey)
+	}
+	if ssodata.TokenName != "foo" {
+		test.Fatalf("Wrong token name: %s", ssodata.TokenName)
+	}
+	if ssodata.TokenSecret != "mTBgLxtTRUdfqewqgrqsvxlijbMWkPBajgKcoZCrDwv" {
+		test.Fatalf("Wrong Token secret: %s", ssodata.TokenSecret)
+	}
+}
+
+func TestSignRequest(test *testing.T) {
+	// Test SignRequest
+	creds := Credentials{
+		"foo@bar.com",
+		"foobarpwd",
+		"token_name",
+		"http://localhost:8123"}
+	ssodata, _ := GetToken(&creds)
+	ssodata.BaseURL = "https://login.staging.ubuntu.com/api/v2/accounts/"
+	request, _ := http.NewRequest(
+		"GET",
+		"https://login.staging.ubuntu.com/api/v2/accounts/"+ssodata.ConsumerKey,
+		nil)
+	ssodata.Sign(request)
+	if !strings.Contains(
+		request.Header["Authorization"][0],
+		`OAuth realm="API"`) {
+		test.Fail()
+	}
+	if !strings.Contains(
+		request.Header["Authorization"][0],
+		`oauth_consumer_key="rfyzhdQ"`) {
+		test.Fail()
+	}
+	if !strings.Contains(
+		request.Header["Authorization"][0],
+		`oauth_token="abcs"`) {
+		test.Fail()
+	}
+	if !strings.Contains(
+		request.Header["Authorization"][0],
+		`oauth_signature="rwDkQkkdfdfdeAslkmmxAOjOAT%26mTBgLxtTRUdfqewqgrqsvx`+
+			`lijbMWkPBajgKcoZCrDwv"`) {
+		test.Fail()
+	}
 }
