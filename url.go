db52de10a1c4ff746eb74461cf89ec93b0697fdc
Jeroen Vermeulen <jtv@canonical.com>
2013-01-21 12:58:31 +0700

    Skeleton: URL normalizers.

---
+++ url.go
@@ -0,0 +1,15 @@
+package usso
+
+func NormalizeMethod(url string) string {
+	return url
+}
+
+
+func NormalizeURL(url string) string {
+	return url
+}
+
+
+func NormalizeParameters(parameters map[string]string) string {
+	return ""
+}
---
+++ url_test.go
@@ -0,0 +1,50 @@
+package usso
+
+import (
+	"launchpad.net/gocheck"
+)
+
+
+func (suite *USSOTestSuite) TestNormalizeMethod(c *gocheck.C) {
+	c.Assert(nil, gocheck.Equals, "TEST THIS")
+}
+
+
+func (suite *USSOTestSuite) TestNormalizeURLReturnsBasicURL(c *gocheck.C) {
+	c.Assert(nil, gocheck.Equals, "TEST THIS")
+}
+
+
+func (suite *USSOTestSuite) TestNormalizeURLStripsStandardPort(c *gocheck.C) {
+	c.Assert(nil, gocheck.Equals, "TEST THIS")
+}
+
+
+func (suite *USSOTestSuite) TestNormalizeURLLeavesNonstandardPort(c *gocheck.C) {
+	c.Assert(nil, gocheck.Equals, "TEST THIS")
+}
+
+
+func (suite *USSOTestSuite) TestNormalizeURLStripsParameters(c *gocheck.C) {
+	c.Assert(nil, gocheck.Equals, "TEST THIS")
+}
+
+
+func (suite *USSOTestSuite) TestNormalizeParametersReturnsParameters(c *gocheck.C) {
+	c.Assert(nil, gocheck.Equals, "TEST THIS")
+}
+
+
+func (suite *USSOTestSuite) TestNormalizeParametersSortsParameters(c *gocheck.C) {
+	c.Assert(nil, gocheck.Equals, "TEST THIS")
+}
+
+
+func (suite *USSOTestSuite) TestNormalizeParametersEscapesParameters(c *gocheck.C) {
+	c.Assert(nil, gocheck.Equals, "TEST THIS")
+}
+
+
+func (suite *USSOTestSuite) TestNormalizeParametersOmitsOAuthSignature(c *gocheck.C) {
+	c.Assert(nil, gocheck.Equals, "TEST THIS")
+}
