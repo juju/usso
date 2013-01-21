712df9c76f4cfa5f05e41058690ff8ea633cfb95
Jeroen Vermeulen <jtv@canonical.com>
2013-01-21 13:36:44 +0700

    Satisfied NormalizeMethod tests.

--- url.go
+++ url.go
@@ -1,7 +1,17 @@
 package usso
 
-func NormalizeMethod(url string) (string, error) {
-	return url, nil
+import (
+	"net/url"
+	"strings"
+)
+
+
+func NormalizeMethod(input_url string) (string, error) {
+	parsed_url, err := url.Parse(input_url)
+	if err != nil {
+		return "ERROR", err
+	}
+	return strings.ToUpper(parsed_url.Scheme), nil
 }
 
 
