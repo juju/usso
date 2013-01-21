4ab2aaef3dc8322bd15540e4f00f365d91622fc3
Jeroen Vermeulen <jtv@canonical.com>
2013-01-21 18:41:42 +0700

    Implemented port-parsing.  Satisfied remaining NormalizeURL tests.

--- url.go
+++ url.go
@@ -16,26 +16,28 @@ func NormalizeMethod(input_url string) (string, error) {
 }
 
 
-func NormalizeURL(input_url string) (string, error) {
-/*
-	standard_ports := map[string]int{
-		"http": 80,
-		"https": 443,
+func normalizeHost(scheme, host_spec string) string {
+	standard_ports := map[string]string{
+		"http": "80",
+		"https": "443",
 	}
-*/
+	host_parts := strings.Split(host_spec, ":")
+	if len(host_parts) == 2 && host_parts[1] == standard_ports[scheme] {
+		// There's a port, but it's the default one.  Leave it out.
+		return host_parts[0]
+	}
+	return host_spec
+}
+
+
+func NormalizeURL(input_url string) (string, error) {
 	parsed_url, err := url.Parse(input_url)
 	if err != nil {
 		return "", err
 	}
 
-/*
-	if parsed_url.Port == standard_ports[parsed_url.Scheme] {
-		port_spec := ""
-	} else {
-		port_spec := fmt.Sprintf(":%v", parsed_url.Port)
-	}
-*/ port_spec := ""
-	normalized_url := fmt.Sprintf("%v://%v%v%v", parsed_url.Scheme, parsed_url.Host, port_spec, parsed_url.Path)
+	host := normalizeHost(parsed_url.Scheme, parsed_url.Host)
+	normalized_url := fmt.Sprintf("%v://%v%v", parsed_url.Scheme, host, parsed_url.Path)
 	return normalized_url, nil
 }
 
