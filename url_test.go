e4dbbfd066e1d0baafc1d52f7627873c8624bd40
Jeroen Vermeulen <jtv@canonical.com>
2013-01-21 13:11:47 +0700

    Implemented most of the sketched tests.

--- url_test.go
+++ url_test.go
@@ -6,37 +6,49 @@ import (
 
 
 func (suite *USSOTestSuite) TestNormalizeMethod(c *gocheck.C) {
-	c.Assert(nil, gocheck.Equals, "TEST THIS")
+	c.Assert(NormalizeMethod("http://example.com/path?query=params"), gocheck.Equals, "HTTP")
+	c.Assert(NormalizeMethod("https://example.com/path?query=params"), gocheck.Equals, "HTTPS")
 }
 
 
 func (suite *USSOTestSuite) TestNormalizeURLReturnsBasicURL(c *gocheck.C) {
-	c.Assert(nil, gocheck.Equals, "TEST THIS")
+	c.Assert(NormalizeURL("http://example.com/path"), gocheck.Equals, "http://example.com/path")
 }
 
 
 func (suite *USSOTestSuite) TestNormalizeURLStripsStandardPort(c *gocheck.C) {
-	c.Assert(nil, gocheck.Equals, "TEST THIS")
+	c.Assert(NormalizeURL("http://example.com:80/"), gocheck.Equals, "http://example.com/path")
+	c.Assert(NormalizeURL("https://example.com:443/"), gocheck.Equals, "https://example.com/path")
 }
 
 
 func (suite *USSOTestSuite) TestNormalizeURLLeavesNonstandardPort(c *gocheck.C) {
-	c.Assert(nil, gocheck.Equals, "TEST THIS")
+	c.Assert(NormalizeURL("http://example.com:8080/"), gocheck.Equals, "http://example.com/path")
 }
 
 
 func (suite *USSOTestSuite) TestNormalizeURLStripsParameters(c *gocheck.C) {
-	c.Assert(nil, gocheck.Equals, "TEST THIS")
+	c.Assert(NormalizeURL("http://example.com/path?query=value&param=arg"), gocheck.Equals, "http://example.com/path")
 }
 
 
 func (suite *USSOTestSuite) TestNormalizeParametersReturnsParameters(c *gocheck.C) {
-	c.Assert(nil, gocheck.Equals, "TEST THIS")
+	c.Assert(NormalizeParameters(map[string]string{"param": "value"}), gocheck.Equals, "param=value")
+}
+
+
+func (suite *USSOTestSuite) TestNormalizeParametersConcatenatesParameters(c *gocheck.C) {
+	c.Assert(NormalizeParameters(map[string]string{"a": "1", "b": "2"}), gocheck.Equals, "a=1&b=2")
 }
 
 
 func (suite *USSOTestSuite) TestNormalizeParametersSortsParameters(c *gocheck.C) {
-	c.Assert(nil, gocheck.Equals, "TEST THIS")
+	params := map[string]string{
+		"b": "x",
+		"a": "y",
+		"c": "z",
+	}
+	c.Assert(NormalizeParameters(params), gocheck.Equals, "a=y&b=x&c=z")
 }
 
 
