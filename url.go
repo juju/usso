b040d2536b818227fdc464e9aaade8aa6bfbd869
Jeroen Vermeulen <jtv@canonical.com>
2013-01-21 13:56:52 +0700

    Satisfied basic URL-parsing tests... but ports don't work. :(

--- url.go
+++ url.go
@@ -1,6 +1,7 @@
 package usso
 
 import (
+	"fmt"
 	"net/url"
 	"strings"
 )
@@ -9,14 +10,33 @@ import (
 func NormalizeMethod(input_url string) (string, error) {
 	parsed_url, err := url.Parse(input_url)
 	if err != nil {
-		return "ERROR", err
+		return "", err
 	}
 	return strings.ToUpper(parsed_url.Scheme), nil
 }
 
 
-func NormalizeURL(url string) (string, error) {
-	return url, nil
+func NormalizeURL(input_url string) (string, error) {
+/*
+	standard_ports := map[string]int{
+		"http": 80,
+		"https": 443,
+	}
+*/
+	parsed_url, err := url.Parse(input_url)
+	if err != nil {
+		return "", err
+	}
+
+/*
+	if parsed_url.Port == standard_ports[parsed_url.Scheme] {
+		port_spec := ""
+	} else {
+		port_spec := fmt.Sprintf(":%v", parsed_url.Port)
+	}
+*/ port_spec := ""
+	normalized_url := fmt.Sprintf("%v://%v%v%v", parsed_url.Scheme, parsed_url.Host, port_spec, parsed_url.Path)
+	return normalized_url, nil
 }
 
 
