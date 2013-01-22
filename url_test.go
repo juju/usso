38cc63e33493141dcabd3e27eb259a993534aa67
Jeroen Vermeulen <jtv@canonical.com>
2013-01-22 10:39:04 +0700

    Use net.url.Values instead of maps, and use C.Check instead of C.Assert.

--- url_test.go
+++ url_test.go
@@ -2,84 +2,85 @@ package usso
 
 import (
 	"launchpad.net/gocheck"
+	"net/url"
 )
 
 
 func (suite *USSOTestSuite) TestNormalizeURLReturnsBasicURL(c *gocheck.C) {
 	output, err := NormalizeURL("http://example.com/path")
-	c.Assert(nil, gocheck.Equals, err)
-	c.Assert(output, gocheck.Equals, "http://example.com/path")
+	c.Check(err, gocheck.Equals, nil)
+	c.Check(output, gocheck.Equals, "http://example.com/path")
 }
 
 
 func (suite *USSOTestSuite) TestNormalizeURLStripsStandardHTTPPort(c *gocheck.C) {
 	output, err := NormalizeURL("http://example.com:80/path")
-	c.Assert(nil, gocheck.Equals, err)
-	c.Assert(output, gocheck.Equals, "http://example.com/path")
+	c.Check(err, gocheck.Equals, nil)
+	c.Check(output, gocheck.Equals, "http://example.com/path")
 }
 
 
 func (suite *USSOTestSuite) TestNormalizeURLStripsStandardHTTPSPort(c *gocheck.C) {
 	output, err := NormalizeURL("https://example.com:443/path")
-	c.Assert(nil, gocheck.Equals, err)
-	c.Assert(output, gocheck.Equals, "https://example.com/path")
+	c.Check(err, gocheck.Equals, nil)
+	c.Check(output, gocheck.Equals, "https://example.com/path")
 }
 
 
 func (suite *USSOTestSuite) TestNormalizeURLLeavesNonstandardPort(c *gocheck.C) {
 	output, err := NormalizeURL("http://example.com:8080/")
-	c.Assert(nil, gocheck.Equals, err)
-	c.Assert(output, gocheck.Equals, "http://example.com:8080/")
+	c.Check(err, gocheck.Equals, nil)
+	c.Check(output, gocheck.Equals, "http://example.com:8080/")
 }
 
 
 func (suite *USSOTestSuite) TestNormalizeURLStripsParameters(c *gocheck.C) {
 	output, err := NormalizeURL("http://example.com/path?query=value&param=arg")
-	c.Assert(nil, gocheck.Equals, err)
-	c.Assert(output, gocheck.Equals, "http://example.com/path")
+	c.Check(err, gocheck.Equals, nil)
+	c.Check(output, gocheck.Equals, "http://example.com/path")
 }
 
 
 func (suite *USSOTestSuite) TestNormalizeParametersReturnsParameters(c *gocheck.C) {
-	output, err := NormalizeParameters(map[string]string{"param": "value"})
-	c.Assert(nil, gocheck.Equals, err)
-	c.Assert(output, gocheck.Equals, "param=value")
+	output, err := NormalizeParameters(url.Values{"param": []string{"value"}})
+	c.Check(err, gocheck.Equals, nil)
+	c.Check(output, gocheck.Equals, "param=value")
 }
 
 
 func (suite *USSOTestSuite) TestNormalizeParametersConcatenatesParameters(c *gocheck.C) {
-	output, err := NormalizeParameters(map[string]string{"a": "1", "b": "2"})
-	c.Assert(nil, gocheck.Equals, err)
-	c.Assert(output, gocheck.Equals, "a=1&b=2")
+	output, err := NormalizeParameters(url.Values{"a": []string{"1"}, "b": []string{"2"}})
+	c.Check(err, gocheck.Equals, nil)
+	c.Check(output, gocheck.Equals, "a=1&b=2")
 }
 
 
 func (suite *USSOTestSuite) TestNormalizeParametersSortsParameters(c *gocheck.C) {
-	params := map[string]string{
-		"b": "x",
-		"a": "y",
-		"c": "z",
+	params := url.Values{
+		"b": []string{"x"},
+		"a": []string{"y"},
+		"c": []string{"z"},
 	}
 	output, err := NormalizeParameters(params)
-	c.Assert(nil, gocheck.Equals, err)
-	c.Assert(output, gocheck.Equals, "a=y&b=x&c=z")
+	c.Check(err, gocheck.Equals, nil)
+	c.Check(output, gocheck.Equals, "a=y&b=x&c=z")
 }
 
 
 func (suite *USSOTestSuite) TestNormalizeParametersEscapesParameters(c *gocheck.C) {
-	output, err := NormalizeParameters(map[string]string{"a&b": "1"})
-	c.Assert(nil, gocheck.Equals, err)
-	c.Assert(output, gocheck.Equals, "a%26b=1")
+	output, err := NormalizeParameters(url.Values{"a&b": []string{"1"}})
+	c.Check(err, gocheck.Equals, nil)
+	c.Check(output, gocheck.Equals, "a%26b=1")
 }
 
 
 func (suite *USSOTestSuite) TestNormalizeParametersOmitsOAuthSignature(c *gocheck.C) {
-	params := map[string]string{
-		"a": "1",
-		"oauth_signature": "foobarsplatszot",
-		"z": "26",
+	params := url.Values{
+		"a": []string{"1"},
+		"oauth_signature": []string{"foobarsplatszot"},
+		"z": []string{"26"},
 	}
 	output, err := NormalizeParameters(params)
-	c.Assert(nil, gocheck.Equals, err)
-	c.Assert(output, gocheck.Equals, "a=1&z=26")
+	c.Check(err, gocheck.Equals, nil)
+	c.Check(output, gocheck.Equals, "a=1&z=26")
 }
